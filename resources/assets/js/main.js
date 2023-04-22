805b8ba94669d25f3cde38ef6ff8328def6e781d
Nisa Delgado <nisadelgado@gmail.com>
2023-04-22 13:16:03 -0400

    Diseño de bus

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -35,5 +35,76 @@ $(document).ready(function () {
         } else {
             $(menu).show();
         }
-    })
+    });
+
+    $('[name=total_seats]').keyup(function () {
+        total_seats = $(this).val();
+        total_seats = parseInt(total_seats);
+
+        design = $('[name=design]').val();
+
+        right = design.split('-')[0];
+        right = parseInt(right);        
+
+        left = design.split('-')[1];
+        left = parseInt(left);
+
+        html = '<table>';
+
+        iterations = total_seats / (right+left);
+        residue = iterations % 1;
+
+        for (i = 0; i <= iterations - 1; i++) {
+            html = html + '<tr>';
+
+            for (j = 0; j <= left - 1; j++) {
+                html = html + '<td><img src="/resources/assets/img/seat.png"></td>';
+            }
+
+            html = html + '<td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td>';
+
+            for (k = 0; k <= right - 1; k++) {
+                html = html + '<td><img src="/resources/assets/img/seat.png"></td>';
+            }
+
+            html = html + '</tr>';
+        }
+
+        if (residue) {
+            iterations = residue * (right+left);
+            html = html + '<tr>';
+
+            if (iterations <= left) {
+                for (i = 0; i <= iterations - 1; i++) {
+                    html = html + '<td><img src="/resources/assets/img/seat.png"></td>';
+                }
+            }
+
+            if (iterations > left) {
+                for (i = 0; i <= left - 1; i++) {
+                    html = html + '<td><img src="/resources/assets/img/seat.png"></td>';
+                }
+
+                html = html + '<td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td>';
+
+                for (i = 0; i <= (iterations - left) - 1; i++) {
+                    html = html + '<td><img src="/resources/assets/img/seat.png"></td>';
+                }
+            }
+
+            html = html + '</tr>';
+        }
+
+        seats = '';
+
+        for (i = 1; i <= total_seats; i++) {
+            seats = seats + i + ',';
+        }
+
+        $('[name=seats_number]').val(seats.substring(0, seats.length - 1));
+
+        html = html + '</table>';
+
+        $('.design').html(html);
+    });
 });
--- resources/views/bus-type/create.blade.php
+++ resources/views/bus-type/create.blade.php
@@ -5,40 +5,48 @@
 
             <div class="card">
                 <div class="card-body">
-                    <div class="form-group">
-                        <label for="type">Tipo</label>
-                        <input type="text" required name="type" class="form-control">
-                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="form-group">
+                                <label for="type">Tipo</label>
+                                <input type="text" required name="type" class="form-control">
+                            </div>
 
-                    <div class="form-group">
-                        <label for="design">Diseño</label>
-                        <select name="design" class="form-control" required>
-                            <option value="2-2">2-2</option>
-                            <option value="2-2">1-1</option>
-                            <option value="2-2">2-1</option>
-                            <option value="2-2">1-2</option>
-                            <option value="2-2">3-2</option>
-                            <option value="2-2">2-3</option>
-                        </select>
-                    </div>
+                            <div class="form-group">
+                                <label for="design">Diseño</label>
+                                <select name="design" class="form-control" required>
+                                    <option value="2-2">2-2</option>
+                                    <option value="1-1">1-1</option>
+                                    <option value="2-1">2-1</option>
+                                    <option value="1-2">1-2</option>
+                                    <option value="3-2">3-2</option>
+                                    <option value="2-3">2-3</option>
+                                </select>
+                            </div>
 
-                    <div class="form-group">
-                        <label for="total_seats">Total de asientos</label>
-                        <input type="text" required name="total_seats" class="form-control">
-                    </div>
+                            <div class="form-group">
+                                <label for="total_seats">Total de asientos</label>
+                                <input type="text" required name="total_seats" class="form-control">
+                            </div>
 
-                    <div class="form-group">
-                        <label for="seats_number">Número de asientos</label>
-                        <textarea name="seats_number" required class="form-control"></textarea>
-                        <label class="text-danger">Valores separados por coma</label>
-                    </div>
+                            <div class="form-group">
+                                <label for="seats_number">Número de asientos</label>
+                                <textarea name="seats_number" required class="form-control"></textarea>
+                                <label class="text-danger">Valores separados por coma</label>
+                            </div>
 
-                    <div class="form-group">
-                        <label for="status">Estado</label>
+                            <div class="form-group">
+                                <label for="status">Estado</label>
 
-                        <div>
-                            <input type="radio" name="status" value="active"> Activo
-                            <input type="radio" name="status" value="inactive"> Inactivo
+                                <div>
+                                    <input type="radio" name="status" value="active"> Activo
+                                    <input type="radio" name="status" value="inactive"> Inactivo
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 ml-5 design">
+                            
                         </div>
                     </div>
 
